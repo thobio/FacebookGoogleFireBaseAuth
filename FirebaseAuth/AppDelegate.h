31c1b6e8b9179a0fd5c37deb6a0710db0866110a
Thobio Joseph <love2code@icloud.com>
2017-08-08 11:53:58 +0530

    Initial Commit

---
+++ FirebaseAuth/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  FirebaseAuth
+//
+//  Created by Thobio Joseph on 08/08/17.
+//  Copyright © 2017 Love2Code. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ FirebaseAuth/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  FirebaseAuth
+//
+//  Created by Thobio Joseph on 08/08/17.
+//  Copyright © 2017 Love2Code. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
